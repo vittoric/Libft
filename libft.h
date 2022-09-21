e158151d61fe00e60515cea27dbd82f7f4fc92c0
Victoria Codreanu <vcodrean@c3r1s6.42madrid.com>
2022-09-21 15:38:42 +0200

    commit

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: vcodrean <vcodrean@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 17:47:55 by vcodrean          #+#    #+#             */
-/*   Updated: 2022/09/21 13:32:53 by vcodrean         ###   ########.fr       */
+/*   Updated: 2022/09/21 15:26:47 by vcodrean         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,4 +46,5 @@ char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
 #endif
