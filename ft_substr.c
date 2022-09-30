a06be52f7ebbe19c9df2a28a2996794bc2b15d7f
Victoria Codreanu <vcodrean@c3r1s5.42madrid.com>
2022-09-30 15:31:55 +0200

    commit

--- ft_substr.c
+++ ft_substr.c
@@ -6,23 +6,12 @@
 /*   By: vcodrean <vcodrean@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 16:29:19 by vcodrean          #+#    #+#             */
-/*   Updated: 2022/09/30 15:22:40 by vcodrean         ###   ########.fr       */
+/*   Updated: 2022/09/30 15:31:48 by vcodrean         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*"ft_substr() returns a pointer to a new string which is a substring of 
- * the string 's' starting at index 'start' and of length 'len'."
- * 
- * The first thing we do is check if the string 's' is NULL. If it is, we return NULL
- * 
- * @param char s The string to be cut.
- * @param unsigned int start the index of the first character to include in the substring.
- * @param size_t len the length of the substring
- * 
- * @return A pointer to a new string.
- */
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*str;
