c78674cdb6ea4008483d581edf45d5177d6ffa26
toricode <93945847+vittoric@users.noreply.github.com>
2022-10-17 16:54:50 +0200

    Update ft_strdup.c

--- ft_strdup.c
+++ ft_strdup.c
@@ -20,16 +20,16 @@
  * 
  * return A pointer to a new string that is a duplicate of the string s1.
  */
-char	*ft_strdup(const char *s1)
+char	*ft_strdup(const char *s1) //string a copiar
 {
 	char	*ptr;
 	int		size;
 
-	size = ft_strlen(s1);
-	ptr = malloc(sizeof(char) * (size + 1));
-	if (ptr == NULL)
-		return (NULL);
-	ft_memcpy(ptr, s1, size);
-	ptr[size] = '\0';
-	return (ptr);
+	size = ft_strlen(s1);  //almaceno la longitud de s1 en ls vsrisblr size
+	ptr = malloc(sizeof(char) * (size + 1)); //ptr = se reserva memoria con malloc de la longitud guardada  + 1 
+	if (ptr == NULL) //si la reserva falla
+		return (NULL); //retorno null
+	ft_memcpy(ptr, s1, size); //copiao mi string a un area de memoria nueva apuntada por ptr, hasta size
+	ptr[size] = '\0'; // se cierra el final del string con un caracter vacio.
+	return (ptr); //devuelve un puntero a una nueva cadena que es un duplicado de la cadena s1
 }
