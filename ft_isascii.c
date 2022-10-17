bbd046180ec60b1e6d38fa3432feb4076e8d4313
toricode <93945847+vittoric@users.noreply.github.com>
2022-10-17 18:04:03 +0200

    Update ft_isascii.c

--- ft_isascii.c
+++ ft_isascii.c
@@ -19,9 +19,9 @@
  * 
  * return 1 if the value is between 0 and 127, 0 otherwise.
  */
-int	ft_isascii(int v)
+int	ft_isascii(int c)
 {
-	if (v >= 0 && v <= 127)
+	if (c >= 0 && c <= 127)
 		return (1);
 	return (0);
 }
