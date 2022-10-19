9fb4eb706bb8135fe01a780fa71c488397fcd45f
toricode <93945847+vittoric@users.noreply.github.com>
2022-10-19 18:31:03 +0200

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -27,14 +27,14 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t		i;
 
-	i = 0;
-	while (i < n && (s1[i] || s2[i]))
+	i = 0;  				// inicializo i en cero
+	while (i < n && (s1[i] || s2[i]))	//mientras i sea menor que el tamño de mi buffer y por s1 de i o sa2 de i exista
 	{
-		if (s1[i] != s2[i])
-			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
-		else if (s1[i] == '\0')
-			return (0);
-i++;
+		if (s1[i] != s2[i])		//si s1 de i es diferente a s2 de i
+			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);	// devuelvo la diferencia de la ultima pocision coincidente
+		else if (s1[i] == '\0')		// si s1 en la pocicion y es nulo
+			return (0);		//devuelvo nulo
+		i++;				// aumento el iterdor		
 	}
-	return (0);
+	return (0);				// devuelvo nulo ( en el caso que no se me cumple el while)
 }
