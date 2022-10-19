fafeba36c06280ab6cc9180c402f6a0e6471b01a
toricode <93945847+vittoric@users.noreply.github.com>
2022-10-19 18:50:20 +0200

    Update ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -23,6 +23,7 @@
  * 
  * @return The difference between the first two bytes that are different.
  */
+
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t					i;
@@ -32,15 +33,16 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	i = 0;
 	str1 = (unsigned const char *)s1;
 	str2 = (unsigned const char *)s2;
-	while (i < n) //mientras i < que el tamaño de mi buffer
+	while (i < n) 					//mientras i < que el tamaño de mi buffer
 	{
-		if (*str1 != *str2)
+		if (*str1 != *str2)			// si primer str es diferente del degundo str
 		{
-			return ((int)(*str1 - *str2));
+			return ((int)(*str1 - *str2)); // devuelve la diferencia entre los dos strings
 		}
-		str1++;
+		str1++;  				//iterammos para poder segir comprobando
 		str2++;
 		i++;
 	}
-	return (0);
+	return (0);  					//devuelve cero si las dos cadenas son idénticas 
+							//(en caso de no cumplir la condicion del while))
 }
