3c2616c89d7be519e4f49e5b9dcec85d0c3b752a
toricode <93945847+vittoric@users.noreply.github.com>
2022-10-19 13:08:38 +0200

    Update ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -25,25 +25,25 @@
  */
 size_t	ft_strlcat(char *dest, const char *src, size_t dstsize)
 {
-	size_t	dstlen;
-	size_t	srclen;
-	size_t	i;
+	size_t	dstlen; //variable para guardar l alongidyd de dest
+	size_t	srclen; //variable para guardar l alongidyd de src
+	size_t	i;  //variable iterador
 
-	dstlen = ft_strlen(dest);
-	srclen = ft_strlen(src);
-	i = 0;
-	if (dstsize == 0)
-		return (srclen);
-	if (dstsize <= dstlen)
-		return (srclen += dstsize);
-	else
-		srclen += dstlen;
-	while (src[i] != '\0' && dstlen < dstsize - 1 && dest != src)
-	{
+	dstlen = ft_strlen(dest); //mide y almacena la longitud de dest.
+	srclen = ft_strlen(src);  //mide y almacena la longitud de src.
+	i = 0;  //inicio el iterador a cero
+	if (dstsize == 0). //control de errores: si el numero de caracteres a copiar es cero
+		return (srclen); //devuelvo la longitud del origen
+	if (dstsize <= dstlen)  //si el tamaño de mi buffer es menor o igual que la longitud de mi destino
+		return (srclen += dstsize); //devuelvo la longitud de mi origen + el tamaño de mi buffer
+	else //si no
+		srclen += dstlen; ////devuelvo la longitud de mi origen + el la longitud de mi destino
+	while (src[i] != '\0' && dstlen < dstsize - 1 && dest != src) // mientras el origen en la posicion i llege al final y
+	{								// la longitud del destino sea menor al tamaño de buffer menos 1 y destino sea diferente al origen
 		dest[dstlen] = src[i];
 		i++;
 		dstlen++;
 	}
 	dest[dstlen] = '\0';
-	return (srclen);
+	return (srclen);  // La longitud de la cadena que intentó crear en dest
 }
